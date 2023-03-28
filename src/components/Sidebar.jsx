2226a7d745c45deedc88178d0f2abf2d630f4b8a
asadparkar <58441390+asadparkar@users.noreply.github.com>
2023-03-28 06:05:35 +0530

    feat: Added Extensions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,15 +9,22 @@ const Sidebar = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-
+  const [extensionClicked,setExtension] = useState(false);
   const handleExplorerClick = ()=>{
     if (isTabletOrMobile){
       return;
+    }
+    if (extensionClicked === true){
+      setExtension(false);
+      return
     }
     setSidebarToggle(!isSidebarToggelled)
   }
   const handlePortfolioClick = ()=>{
     setPortfoioToggled(!isPortfolioToggelled);
+  }
+  const handleExtensionClick = ()=>{
+    setExtension(true)
   }
 
   useEffect(()=>{
@@ -32,15 +39,18 @@ const Sidebar = () => {
         <div className='explorer' style={{cursor:'pointer'}}>
           <svg onClick={handleExplorerClick} width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="rgb(106, 115, 125)" class="Sidebar_icon__czDJe"><path d="M17.5 0h-9L7 1.5V6H2.5L1 7.5v15.07L2.5 24h12.07L16 22.57V18h4.7l1.3-1.43V4.5L17.5 0zm0 2.12l2.38 2.38H17.5V2.12zm-3 20.38h-12v-15H7v9.07L8.5 18h6v4.5zm6-6h-12v-15H16V6h4.5v10.5z"></path></svg>
           </div>
-          <div style={{marginTop:'75vh',cursor:'pointers'}} onClick={()=>navigate('/settings')}>
+          <div>
+          <svg onClick={handleExtensionClick} style={{fill:'white',opacity:0.2,marginTop:'15px'}} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11 11h-11v-11h11v11zm13 0h-11v-11h11v11zm-13 13h-11v-11h11v11zm13 0h-11v-11h11v11z"/></svg>
+          </div>
+          <div style={{marginTop:'72vh',cursor:'pointers'}} onClick={()=>navigate('/settings')}>
             <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="rgba(225, 228, 232,0.5)" class="Sidebar_icon__czDJe"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.85 8.75l4.15.83v4.84l-4.15.83 2.35 3.52-3.43 3.43-3.52-2.35-.83 4.15H9.58l-.83-4.15-3.52 2.35-3.43-3.43 2.35-3.52L0 14.42V9.58l4.15-.83L1.8 5.23 5.23 1.8l3.52 2.35L9.58 0h4.84l.83 4.15 3.52-2.35 3.43 3.43-2.35 3.52zm-1.57 5.07l4-.81v-2l-4-.81-.54-1.3 2.29-3.43-1.43-1.43-3.43 2.29-1.3-.54-.81-4h-2l-.81 4-1.3.54-3.43-2.29-1.43 1.43L6.38 8.9l-.54 1.3-4 .81v2l4 .81.54 1.3-2.29 3.43 1.43 1.43 3.43-2.29 1.3.54.81 4h2l.81-4 1.3-.54 3.43 2.29 1.43-1.43-2.29-3.43.54-1.3zm-8.186-4.672A3.43 3.43 0 0112 8.57 3.44 3.44 0 0115.43 12a3.43 3.43 0 11-5.336-2.852zm.956 4.274c.281.188.612.288.95.288A1.7 1.7 0 0013.71 12a1.71 1.71 0 10-2.66 1.422z"></path></svg>
           </div>
       </div>
-      {isSidebarToggelled &&  <div style={{background:'#252526', width:'250px', color:'white'}}>
+      {isSidebarToggelled && <div style={{background:'#252526', width:'250px', color:'white'}}>
         <div style={{height:'40px', fontWeight:'300', padding:'10px', fontSize:'13px'}}>
-          EXPLORER
+          {extensionClicked?'EXTENSIONS':'EXPLORER'}
         </div>
-        <div className='portfolio' style={{padding:'8px', cursor:'pointer'}}>
+        {!extensionClicked && <div className='portfolio' style={{padding:'8px', cursor:'pointer'}}>
         <p style={{fontSize:'15px', fontFamily:'Consolas',fontWeight:'600'}} onClick={handlePortfolioClick} > > PORTFOLIO</p>
            {isPortfolioToggelled && 
             <div className='portfolio-items' style={{marginLeft:'15px'}} >
@@ -78,7 +88,12 @@ const Sidebar = () => {
               </div>
             </div>
            }
+        </div>}
+        {extensionClicked &&
+        <div style={{padding:'15px'}}>
+          Coming Soon...
         </div>
+        }
       </div> }
     </div>
   )
